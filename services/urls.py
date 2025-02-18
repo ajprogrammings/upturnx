5373e9dd83adc52bcce33a3bae2b4bda1b2629a1
Arjun Sharma <79274100+ajprogrammings@users.noreply.github.com>
2025-02-18 11:40:48 +0530

    Delete services directory

--- services/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import services_page
-
-urlpatterns = [
-    path('', services_page, name='services'),  # URL for Services Page
-]
--- services/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def services_page(request):
-    return render(request, 'services/services.html')
