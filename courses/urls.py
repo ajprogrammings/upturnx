06ac61c04a628dfec59c264cfed34e2346cc6e90
Arjun Sharma <79274100+ajprogrammings@users.noreply.github.com>
2025-02-18 11:35:57 +0530

    Delete courses directory

--- courses/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import courses_list
-
-urlpatterns = [
-    path('', courses_list, name='courses'),  # This URL pattern is needed
-]
--- courses/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def courses_list(request):
-    return render(request, 'courses/courses.html')
